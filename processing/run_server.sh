a39d0febbf1996396303d14d0a4013515f270c72
Zahid Hossain <zhossain@stanford.edu>
2017-02-06 16:58:51 -0800

    changed all the run scripts to avoid automatic logging

--- processing/run_server.sh
+++ processing/run_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-forever -c "node --max-old-space-size=8192" -o out.log -e err.log  procApp.js
+#forever -c "node --max-old-space-size=8192" -o out.log -e err.log  procApp.js
+forever -c "node --max-old-space-size=8192" procApp.js
--- server/run_server.sh
+++ server/run_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-forever -c "node --max-old-space-size=8192" -o out.log -e err.log  app.js
+#forever -c "node --max-old-space-size=8192" -o out.log -e err.log  app.js
+forever -c "node --max-old-space-size=8192" app.js
