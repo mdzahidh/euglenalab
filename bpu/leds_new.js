085d08e59993fb060c43646d79fe8134d5e91893
githubchik <tahrina007@yahoo.com>
2017-03-09 14:41:02 -0800

    Fixing conversion method and supporting floating input

--- bpu/leds_new.js
+++ bpu/leds_new.js
@@ -7,63 +7,42 @@ rpi.wiringPiSPIDataRW(0,rst_buf);
 var board = {};
 
  
-    board.ledsSet=function(topValue, rightValue, bottomValue, leftValue) {
-      topValue = "0x" + topValue;
-      topValue = parseInt(topValue,16);
-      topValue = topValue.toString(16);
-
-      rightValue = "0x" + rightValue;
-      rightValue = parseInt(rightValue,16);
-      rightValue = rightValue.toString(16);
-
-      bottomValue = "0x" + bottomValue;
-      bottomValue = parseInt(bottomValue,16);
-      bottomValue = bottomValue.toString(16);
-
-      leftValue = "0x" + leftValue;
-      leftValue = parseInt(leftValue,16);
-      leftValue = leftValue.toString(16);
-
-      var buf = new Buffer ([0x11,leftValue.toString(16),0x11,bottomValue.toString(16),0x11,rightValue.toString(16),0x11,topValue.toString(16)]);
-      console.log (buf); 
+    board.ledsSet=function(topValue, rightValue, bottomValue, leftValue) {   
+      topValue = Math.round(( topValue / 100.0 ) * 255.0);
+      rightValue = Math.round(( rightValue / 100.0 ) * 255.0);
+      bottomValue = Math.round(( bottomValue / 100.0 ) * 255.0);
+      leftValue = Math.round(( leftValue / 100.0 ) * 255.0);
+      var buf = new Buffer ([0x11,leftValue,0x11,bottomValue,0x11,rightValue,0x11,topValue]); 
       rpi.wiringPiSPIDataRW(0,buf);
     };
    
-    board.diffuserSet=function(diffuserValue) {
-      diffuserValue = "0x" + diffuserValue;
-      diffuserValue = parseInt(diffuserValue,16);
-      diffuserValue = diffuserValue.toString(16);
-      var buf = new Buffer ([0x00,0x00,0x00,0x00,0x00,0x00,0x12,diffuserValue.toString(16)]); 
+    board.diffuserSet=function(diffuserValue) { 
+      diffuserValue = Math.round(( diffuserValue / 100.0 ) * 255.0);
+      var buf = new Buffer ([0x00,0x00,0x00,0x00,0x00,0x00,0x12,diffuserValue]); 
       rpi.wiringPiSPIDataRW(0,buf);
     };
   
     board.backlightSet=function(backlightValue) {
-      backlightValue = "0x" + backlightValue;
-      backlightValue = parseInt(backlightValue,16);
-      backlightValue = backlightValue.toString(16);
-      var buf = new Buffer ([0x00,0x00,0x00,0x00,0x12,backlightValue.toString(16),0x00,0x00]); 
+      backlightValue = Math.round(( backlightValue / 100.0 ) * 255.0);  
+      var buf = new Buffer ([0x00,0x00,0x00,0x00,0x12,backlightValue,0x00,0x00]); 
       rpi.wiringPiSPIDataRW(0,buf);
     };    
 
     board.culturelightSet=function(culturelightValue) {
-      culturelightValue = "0x" + culturelightValue;
-      culturelightValue = parseInt(culturelightValue,16);
-      culturelightValue = culturelightValue.toString(16);
-      var buf = new Buffer ([0x00,0x00,0x12,culturelightValue.toString(16),0x00,0x00,0x00,0x00]); 
+      culturelightValue = Math.round(( culturelightValue / 100.0 ) * 255.0);
+      var buf = new Buffer ([0x00,0x00,0x12,culturelightValue,0x00,0x00,0x00,0x00]); 
       rpi.wiringPiSPIDataRW(0,buf);
     };
   
     board.ambientlightSet=function(ambientlightValue) {
-      ambientlightValue = "0x" + ambientlightValue;
-      ambientlightValue = parseInt(ambientlightValue,16);
-      ambientlightValue = ambientlightValue.toString(16);
+      ambientlightValue = Math.round(( ambientlightValue / 100.0 ) * 255.0);
       var buf = new Buffer ([0x12,ambientlightValue,0x00,0x00,0x00,0x00,0x00,0x00]); 
       rpi.wiringPiSPIDataRW(0,buf);
     }; 
 
 
-board.ledsSet(255,255,255,255);
-board.diffuserSet(255);
-board.backlightSet(255);
-board.culturelightSet(255);
-board.ambientlightSet(255);
+board.ledsSet(100,100,100,100);
+board.diffuserSet(100);
+board.backlightSet(100);
+board.culturelightSet(100);
+board.ambientlightSet(100);
